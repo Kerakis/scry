8c9d22e981f69490ec4101e7a06c06125d7e30c3
Rob Upchurch <rlupchurch3@gmail.com>
2026-06-16 17:42:37 -0400

    refactor: replace lodash shuffle with native Fisher-Yates

--- src/lib/cardUtils.js
+++ src/lib/cardUtils.js
@@ -1,9 +1,22 @@
-import { shuffle } from 'lodash';
 import { FORMATS } from './constants.js';
 
 /**
  * @typedef {{ id: string, image_uris?: Record<string, string>, name: string, scryfall_uri: string }} Card
  */
+
+/**
+ * @param {T[]} array
+ * @returns {T[]}
+ * @template T
+ */
+function shuffle(array) {
+  const result = [...array];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = crypto.getRandomValues(new Uint32Array(1))[0] % (i + 1);
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+}
 
 /** @param {any[]} allCards */
 export function buildFormatCardMap(allCards) {
